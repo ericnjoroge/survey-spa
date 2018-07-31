088bbfa55f3e8cd0c2eabdca1f52625528ea5867
Data Ricky <ericnjoroge3@gmail.com>
2018-07-31 23:02:25 +0300

    added login and register methods

--- backend/surveyapi/api.py
+++ backend/surveyapi/api.py
@@ -4,15 +4,39 @@ api.py
 requests and responses
 """
 
-from flask import Blueprint, jsonify, request
-from .models import db, Survey, Question, Choice
+from functools import wraps
+from datetime import datetime, timedelta
+
+import jwt
+
+from flask import Blueprint, jsonify, request, current_app
+from .models import db, Survey, Question, Choice, User
 
 api = Blueprint('api',__name__)
 
-@api.route('/hello/<string:name>/')
-def say_hello(name):
-    response = { 'msg': "Hello {}".format(name) }
-    return jsonify(response)
+@api.route('/register/',methods=('POST',))
+def register():
+    data = request.get_json()
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
+
+@api.route('/login/', methods=('POST',))
+def login():
+    data = request.get_json()
+    user = User.authenticate(**data)
+
+    if not user:
+        return jsonify({ 'message': 'Invalid credentials', 'authenticated': False }), 401
+
+    token = jwt.encode({
+        'sub': user.email,
+        'iat': datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=30)
+    }, current_app.config['SECRET_KEY'])
+
+    return jsonify({ 'token': token.decode('UTF-8') })
 
 @api.route('/surveys/',methods=('GET','POST'))
 def surveys():
