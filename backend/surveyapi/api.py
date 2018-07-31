f02d8b090e7889cf7738095965b9d472e0059df1
Data Ricky <ericnjoroge3@gmail.com>
2018-07-31 23:45:09 +0300

    added an auhentication decorator to protect the create survey view

--- backend/surveyapi/api.py
+++ backend/surveyapi/api.py
@@ -38,7 +38,41 @@ def login():
 
     return jsonify({ 'token': token.decode('UTF-8') })
 
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registration and/or authentication required ',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) !=2:
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+
+            user = User.query.filter_by(email=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
+
 @api.route('/surveys/', methods=('POST',))
+@token_required
 def create_survey(current_user):
     data = request.get_json()
     survey = Survey(name=data['name'])
