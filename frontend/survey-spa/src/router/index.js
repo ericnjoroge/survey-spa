73e1e82016004b5158cb0bee2b029aa380f82070
Data Ricky <ericnjoroge3@gmail.com>
2018-08-01 03:04:09 +0300

    added the route guard to the new survey component

--- frontend/survey-spa/src/router/index.js
+++ frontend/survey-spa/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 import Home from '@/components/Home';
 import Survey from '@/components/Survey';
 import NewSurvey from '@/components/NewSurvey'
+import Login from '@/components/Login'
+import store from '@/store'
 
 Vue.use(Router);
 
@@ -21,7 +23,19 @@ export default new Router({
     {
       path: '/surveys/',
       name: 'NewSurvey',
-      component: NewSurvey
+      component: NewSurvey,
+      beforeEnter(to, from, next) {
+        if (!store.getters.isAuthenticated) {
+          next('/login')
+        } else {
+          next()
+        }
+      }
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
   ],
 });
